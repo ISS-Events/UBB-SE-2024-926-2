f4a978718d7e16101677f9fb11d585eef5229ebe
victors3136 <victor.selegean@stud.ubbcluj.ro>
2024-05-20 15:19:40 +0300

    ugaga u ga ga

---
+++ CodeBuddies/Data/DatabaseApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeBuddies.Data
+{
+    internal class DatabaseApplicationContext : DbContext
+    {
+        public DatabaseApplicationContext(DbContextOptions<DatabaseApplicationContext> options)
+        : base(options)
+        {
+        }
+        //public DbSet<> Customers { get; set; }
+        //// Other DbSets for your entities
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure entity filters and constraints here
+        }
+    }
+}
