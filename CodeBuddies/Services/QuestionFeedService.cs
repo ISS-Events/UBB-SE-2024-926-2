60b2d521b6ef2d9ba4d3f03ed29e2ba2fdc7f4d1
victors3136 <victor.selegean@stud.ubbcluj.ro>
2024-05-22 23:12:56 +0300

    Step 3: Prepare Repository Injection for Services

--- CodeBuddies/Services/QuestionFeedService.cs
+++ CodeBuddies/Services/QuestionFeedService.cs
@@ -10,15 +10,19 @@ namespace CodeBuddies.Services
 {
     internal class QuestionFeedService : IQuestionFeedService
     {
+        #region Fields
         private readonly IQuestionFeedRepository repository;
 
         private readonly List<Question> currentQuestions;
+        #endregion
+        #region Constructor
         public QuestionFeedService(IQuestionFeedRepository repository)
         {
             this.repository = repository;
             currentQuestions = GetAllQuestions();
         }
-
+        #endregion
+        #region Methods
         public User GetUser(long userId)
         {
             return repository.GetUser(userId);
@@ -174,7 +178,9 @@ namespace CodeBuddies.Services
         {
             long userID = IDGenerator.RandomLong();
             long questionId = IDGenerator.RandomLong();
-            Question question = new QuestionBuilder().Begin().SetId(questionId).SetUserId(userID).SetContent(content).SetCategory(category).SetTitle(title).End();
+            Question question = new QuestionBuilder().Begin()
+                .SetId(questionId).SetUserId(userID).SetContent(content).SetCategory(category).SetTitle(title)
+                .End();
             repository.AddQuestion(question);
         }
 
@@ -221,5 +227,6 @@ namespace CodeBuddies.Services
                 .Where(QuestionIsPostedWithinPreviousCalendarYear)
                 .Count();
         }
+        #endregion
     }
 }
